539a0a93bf8db9a9bb74eba5c60bab410076a95a
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-04 19:10:34 +0200

    Calculate three subject Average of a student
    Use scanner import to prompt the student three times. Then we sum the three grades, divide by 3 and print the students average.

--- Java/GradeAverageCalculator/src/main/java/com/mycompany/gradeaveragecalculator/GradeAverageCalculator.java
+++ Java/GradeAverageCalculator/src/main/java/com/mycompany/gradeaveragecalculator/GradeAverageCalculator.java
@@ -1,16 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.gradeaveragecalculator;
 
-/**
- *
- * @author CALBIX
- */
+// Import scanner package
+import java.util.Scanner;
+
 public class GradeAverageCalculator {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        // Assign scanner object to a variable to call later
+        Scanner userInput = new Scanner(System.in);
+        
+        // Give context to the user
+        System.out.println("Let's calculate the average of three subjects!");
+        
+        // Ask the user to enter their grades
+        System.out.println("Enter your first subjects grade");
+        // Prompt the user and assign the data back to a var of type int
+        int subjectOne = userInput.nextInt();
+        
+        System.out.println("Enter your second subjects grade");
+        int subjectTwo = userInput.nextInt();
+        
+        System.out.println("Enther your third subjects grade");
+        int subjectThree = userInput.nextInt();
+        
+        // Sum all three grades, divide by 3 and assign the data back to averageGrade
+        int averageGrade = (subjectOne + subjectTwo + subjectThree) / 3;
+        
+        // Print the users averageGrade
+        System.out.println("Your average grade is: " + averageGrade);
     }
 }
