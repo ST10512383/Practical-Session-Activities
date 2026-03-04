b68a1d480f110433339b093620b3d714a1647bcd
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-04 20:45:07 +0200

    Calculate user interest
    Calculate the user interest by asking for three inputs. Then we do a calculation and assign the total interest back to a new variable. We display the users Total Interest in a string

--- Java/BankInterestCalculator/src/main/java/com/mycompany/bankinterestcalculator/BankInterestCalculator.java
+++ Java/BankInterestCalculator/src/main/java/com/mycompany/bankinterestcalculator/BankInterestCalculator.java
@@ -7,13 +7,25 @@ public class BankInterestCalculator {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         
+        // Ask user to enter Principal Amount
         System.out.println("Enter your Principal Amount");
         int principalAmount = userInput.nextInt();
         
+        // Ask user to enter Interest Rate
         System.out.println("Enter your Interest Rate");
-        double interestRate = userInput.nextInt();
+        int interestRate = userInput.nextInt();
         
-        System.out.println("Enter your Intrest Years");
+        // Ask user to enter total interest years
+        System.out.println("Enter your Interest Years");
         int totalInterestYears = userInput.nextInt();
+        
+        // Calculate interest rates and assing it to a variable
+        int totalInterest = (principalAmount * interestRate * totalInterestYears) / 100;
+        
+        // Print users total interest rate
+        System.out.println("Your total interest over " + totalInterestYears + " years " + " is R" + totalInterest);
+        
+        // Close scanner
+        userInput.close();
     }
 }
