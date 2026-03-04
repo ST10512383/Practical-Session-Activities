3de7d03396dc83b22c7dc6700ad19b1e3979f0e0
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-04 20:13:12 +0200

    Update BankInterestCalculator.java

--- Java/BankInterestCalculator/src/main/java/com/mycompany/bankinterestcalculator/BankInterestCalculator.java
+++ Java/BankInterestCalculator/src/main/java/com/mycompany/bankinterestcalculator/BankInterestCalculator.java
@@ -3,6 +3,5 @@ package com.mycompany.bankinterestcalculator;
 public class BankInterestCalculator {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
     }
 }
