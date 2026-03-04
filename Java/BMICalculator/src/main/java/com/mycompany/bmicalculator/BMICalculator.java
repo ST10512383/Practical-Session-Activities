32e810bfb1a0c3c514bcdda0e8c90f32d6cac253
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-04 20:58:38 +0200

    Calculate user's Body Mass Index
    Import java scanner to take user input. Ask user their weight and height and assign each to a variable. Calculate the users BMI and assign it back to a new variable of type double. Print users BMI

--- Java/BMICalculator/src/main/java/com/mycompany/bmicalculator/BMICalculator.java
+++ Java/BMICalculator/src/main/java/com/mycompany/bmicalculator/BMICalculator.java
@@ -1,5 +1,6 @@
 package com.mycompany.bmicalculator;
 
+// Import Scanner
 import java.util.Scanner;
 
 public class BMICalculator {
@@ -7,10 +8,20 @@ public class BMICalculator {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         
+        // Ask user to input their weight
         System.out.println("What is your weight in Kg?");
+        // Assign weight to variable
         int weight = userInput.nextInt();
         
+        // Ask user to input their height
         System.out.println("What is your height in meters?");
+        // Assing height to variable
         double height = userInput.nextDouble();
+        
+        // Calculate bodyMassIndex
+        double bodyMassIndex = weight / (height * height);
+        
+        // Print users BMI
+        System.out.println("Your BMI is: " + bodyMassIndex);
     }
 }
