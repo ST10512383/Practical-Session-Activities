47ccb23183997863195d57701468e81c36b25998
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-04 19:48:33 +0200

    Convert user currency to EUR and GBP
    Use scanner import to accept input from the user and print the currency in multiple converted ones.

--- Java/CurrencyConverter/src/main/java/com/mycompany/currencyconverter/CurrencyConverter.java
+++ Java/CurrencyConverter/src/main/java/com/mycompany/currencyconverter/CurrencyConverter.java
@@ -1,8 +1,24 @@
 package com.mycompany.currencyconverter;
 
+// Import Java Scanner package
+import java.util.Scanner;
+
 public class CurrencyConverter {
 
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
         
+        // Asks user total amount they want to convert
+        System.out.println("Enter total amount you want to convert");
+        
+        // Assigns user input to a variable
+        double amountInUSD = userInput.nextDouble();
+        
+        // Multiply and assign to a new var for each currency
+        double amountInEUR = amountInUSD * 0.92;
+        double amountInGBP = amountInUSD * 0.75;
+        
+        // Converts USD to EUR and GBP. Use escape sequence to print each value on a new line
+        System.out.println("Your amount in USD " + amountInUSD + "\n" + "Converted to EUR " + amountInEUR + "\n" + "Converted to GBP " + amountInGBP);
     }
 }
