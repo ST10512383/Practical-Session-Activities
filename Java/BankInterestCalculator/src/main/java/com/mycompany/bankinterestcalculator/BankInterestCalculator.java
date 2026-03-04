088d6d477de594480cb112101de3d7c6576f470a
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-04 20:21:46 +0200

    Add questions and user input

--- Java/BankInterestCalculator/src/main/java/com/mycompany/bankinterestcalculator/BankInterestCalculator.java
+++ Java/BankInterestCalculator/src/main/java/com/mycompany/bankinterestcalculator/BankInterestCalculator.java
@@ -6,5 +6,14 @@ public class BankInterestCalculator {
 
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
+        
+        System.out.println("Enter your Principal Amount");
+        int principalAmount = userInput.nextInt();
+        
+        System.out.println("Enter your Interest Rate");
+        double interestRate = userInput.nextInt();
+        
+        System.out.println("Enter your Intrest Years");
+        int totalInterestYears = userInput.nextInt();
     }
 }
