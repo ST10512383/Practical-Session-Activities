eef98470780b9f8519d823420f6d84aa4f254f42
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-04 20:15:02 +0200

    Add scanner import

--- Java/BankInterestCalculator/src/main/java/com/mycompany/bankinterestcalculator/BankInterestCalculator.java
+++ Java/BankInterestCalculator/src/main/java/com/mycompany/bankinterestcalculator/BankInterestCalculator.java
@@ -1,7 +1,10 @@
 package com.mycompany.bankinterestcalculator;
 
+import java.util.Scanner;
+
 public class BankInterestCalculator {
 
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
     }
 }
