4339ce954bbf4096cbb802e211c57a44e3500cc9
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-04 20:53:29 +0200

    Add scanner import, statements and variables

--- Java/BMICalculator/src/main/java/com/mycompany/bmicalculator/BMICalculator.java
+++ Java/BMICalculator/src/main/java/com/mycompany/bmicalculator/BMICalculator.java
@@ -1,8 +1,16 @@
 package com.mycompany.bmicalculator;
 
+import java.util.Scanner;
+
 public class BMICalculator {
 
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
         
+        System.out.println("What is your weight in Kg?");
+        int weight = userInput.nextInt();
+        
+        System.out.println("What is your height in meters?");
+        double height = userInput.nextDouble();
     }
 }
