d12046a69bc6d0aa050fb8810156539d8031cb36
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-04 18:50:03 +0200

    Create GradeAverageCalculator.java

---
+++ Java/GradeAverageCalculator/src/main/java/com/mycompany/gradeaveragecalculator/GradeAverageCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gradeaveragecalculator;
+
+/**
+ *
+ * @author CALBIX
+ */
+public class GradeAverageCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
