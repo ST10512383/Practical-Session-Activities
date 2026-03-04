63874f196c6231008ec6bba82e55703d5bd48c24
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-04 20:12:48 +0200

    Create BankInterestCalculator.java

---
+++ Java/BankInterestCalculator/src/main/java/com/mycompany/bankinterestcalculator/BankInterestCalculator.java
@@ -0,0 +1,8 @@
+package com.mycompany.bankinterestcalculator;
+
+public class BankInterestCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
