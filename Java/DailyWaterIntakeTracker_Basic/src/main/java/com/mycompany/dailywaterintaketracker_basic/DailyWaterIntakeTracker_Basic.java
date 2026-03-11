5183aec0b2ee0d7b5ebe0101130657ed98d16ec7
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-11 20:10:22 +0200

    Add basic structure

--- Java/DailyWaterIntakeTracker_Basic/src/main/java/com/mycompany/dailywaterintaketracker_basic/DailyWaterIntakeTracker_Basic.java
+++ Java/DailyWaterIntakeTracker_Basic/src/main/java/com/mycompany/dailywaterintaketracker_basic/DailyWaterIntakeTracker_Basic.java
@@ -4,5 +4,21 @@ public class DailyWaterIntakeTracker_Basic {
 
     public static void main(String[] args) {
         
+        double morning = 0;
+        double afternoon = 0;
+        double evening = 0;
+        
+    }
+    
+    public static void getInput(){
+        
+    }
+    
+    public static void calculateTotal(){
+        
+    }
+    
+    public static void displayResult(){
+        
     }
 }
