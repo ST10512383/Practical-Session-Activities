7097ef53e592b97c76b21fd8a6456d31e9cae633
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-12 21:36:58 +0200

    Move variables out of main for Scope

--- Java/DailyWaterIntakeTracker_Basic/src/main/java/com/mycompany/dailywaterintaketracker_basic/DailyWaterIntakeTracker_Basic.java
+++ Java/DailyWaterIntakeTracker_Basic/src/main/java/com/mycompany/dailywaterintaketracker_basic/DailyWaterIntakeTracker_Basic.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class DailyWaterIntakeTracker_Basic {
 
     private static final Scanner input = new Scanner(System.in);
+    
+    private double morning = 0;
+    private double afternoon = 0;
+    private double evening = 0;
+    
     public static void main(String[] args) {
         
-        double morning = 0;
-        double afternoon = 0;
-        double evening = 0;
+        
         
     }
     
