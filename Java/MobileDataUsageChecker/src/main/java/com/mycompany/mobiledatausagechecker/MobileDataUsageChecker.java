2e243faa82854b31fdc0fd382282fa19756008bc
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-21 18:12:25 +0200

    Add constant variable
    Added DATA_LIMIT as a constant variable

--- Java/MobileDataUsageChecker/src/main/java/com/mycompany/mobiledatausagechecker/MobileDataUsageChecker.java
+++ Java/MobileDataUsageChecker/src/main/java/com/mycompany/mobiledatausagechecker/MobileDataUsageChecker.java
@@ -5,6 +5,9 @@ import javax.swing.JOptionPane;
 
 public class MobileDataUsageChecker {
 
+    // Declare a constant to hold a data limit
+    final static double DATA_LIMIT = 10.0;
+    
     public static void main(String[] args) {
     public static void startApp(){
     }
