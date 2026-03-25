73cfc1bf0550a068aecebddfa92de18b5cf82366
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-25 15:24:08 +0200

    Prompt the user for input

--- Java/FuelCostEstimator/src/main/java/com/mycompany/fuelcostestimator/FuelCostEstimator.java
+++ Java/FuelCostEstimator/src/main/java/com/mycompany/fuelcostestimator/FuelCostEstimator.java
@@ -10,5 +10,13 @@ public class FuelCostEstimator {
         // Instantiate Scanner
         Scanner userInput = new Scanner(System.in);
         
+        // Prompt the user for information
+        System.out.print("Enter distance: ");
+        
+        System.out.print("Enter fuel consumption (km per litre): ");
+        
+        System.out.print("Enter fuel price per litre: ");
+        
+    }
     }
 }
