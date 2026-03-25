f4f0ffdf24121d1ad94418cf2c091d7f97393b60
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-25 15:04:35 +0200

    Import and instantiate Scanner

--- Java/FuelCostEstimator/src/main/java/com/mycompany/fuelcostestimator/FuelCostEstimator.java
+++ Java/FuelCostEstimator/src/main/java/com/mycompany/fuelcostestimator/FuelCostEstimator.java
@@ -1,7 +1,14 @@
 package com.mycompany.fuelcostestimator;
 
+// Import Scanner package
+import java.util.Scanner;
+
 public class FuelCostEstimator {
 
     public static void main(String[] args) {
+        
+        // Instantiate Scanner
+        Scanner userInput = new Scanner(System.in);
+        
     }
 }
