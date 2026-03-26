c95ab81f29eaaff191cde82e935886edeb75a990
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-26 12:50:01 +0200

    Import and Instantiate Scanner Object

--- Java/Methods-and-Arithmetic-Operators/ShopChangeCalculator/src/main/java/com/mycompany/shopchangecalculator/ShopChangeCalculator.java
+++ Java/Methods-and-Arithmetic-Operators/ShopChangeCalculator/src/main/java/com/mycompany/shopchangecalculator/ShopChangeCalculator.java
@@ -1,7 +1,13 @@
 package com.mycompany.shopchangecalculator;
 
+// Import Scanner package
+import java.util.Scanner;
+
 public class ShopChangeCalculator {
 
     public static void main(String[] args) {
+        
+        // Instantiate Scanner object
+        Scanner userInput = new Scanner(System.in);
     }
 }
