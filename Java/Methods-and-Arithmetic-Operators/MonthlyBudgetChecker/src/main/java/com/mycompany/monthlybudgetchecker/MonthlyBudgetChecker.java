4cc17858cacd025cf0cb09f628035e626db37181
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-26 11:23:23 +0200

    Add Ask user input
    Ask the user for their expenses

--- Java/Methods-and-Arithmetic-Operators/MonthlyBudgetChecker/src/main/java/com/mycompany/monthlybudgetchecker/MonthlyBudgetChecker.java
+++ Java/Methods-and-Arithmetic-Operators/MonthlyBudgetChecker/src/main/java/com/mycompany/monthlybudgetchecker/MonthlyBudgetChecker.java
@@ -7,6 +7,12 @@ public class MonthlyBudgetChecker {
 
     public static void main(String[] args) {
         
+        // Ask the user their expenses through the use of JOptionPane
+        double monthlyAllowance = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter your monthly allowance"));
+        double foodExpenses = Double.parseDouble(JOptionPane.showInputDialog(null, "What are your food expenses?"));
+        double transportExpenses = Double.parseDouble(JOptionPane.showInputDialog(null, "What are your transport expenses?"));
+        double entertainmentExpenses = Double.parseDouble(JOptionPane.showInputDialog(null, "What are your entertainment expenses?"));
+        
     }
     
     public static double calculateExpenses(double food, double transport, double entertainment) {
