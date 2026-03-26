6e4a32aefb11337b98d446492b4b3f4d2ca53b75
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-26 11:13:09 +0200

    Import JOptionPane for GUI

--- Java/Methods-and-Arithmetic-Operators/MonthlyBudgetChecker/src/main/java/com/mycompany/monthlybudgetchecker/MonthlyBudgetChecker.java
+++ Java/Methods-and-Arithmetic-Operators/MonthlyBudgetChecker/src/main/java/com/mycompany/monthlybudgetchecker/MonthlyBudgetChecker.java
@@ -1,5 +1,8 @@
 package com.mycompany.monthlybudgetchecker;
 
+// Import JOptionPane package
+import javax.swing.JOptionPane;
+
 public class MonthlyBudgetChecker {
 
     public static void main(String[] args) {
